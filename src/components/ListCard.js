73a65a3a999c220d978ab44184109839627e2292
disaacjm <disaacjm@gmail.com>
2023-07-05 16:29:34 +0100

    implement styles

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Card, Button, Row, Col } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
 function ListCard({ title, description, date, _id }) {
   return (
