3d437fc4e9c723454da0d97e077289560124c205
Lucas Silva <gaviao.lucas@gmail.com>
2023-09-20 14:56:08 +0100

    theme fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,76 +1,63 @@
 import { AuthContext } from "../context/auth.context";
 import { useContext } from "react";
 import { ThemeContext } from "../context/theme.context";
-import { useNavigate } from "react-router-dom";
 import { Nav, Button } from "react-bootstrap";
+import Hamburger from "./Hamburger";
+import { useState } from "react";
 
 function Navbar() {
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
-  const navigate = useNavigate();
+  const { isLoggedIn, logOutUser } = useContext(AuthContext);
 
   const { theme, toggleTheme } = useContext(ThemeContext);
 
-  const routeChangeAbout = () => {
-    navigate("/about");
-  };
-  const routeChangeLists = () => {
-    navigate("/lists");
-  };
-  const routeChangeProducts = () => {
-    navigate("/products");
-  };
-  const routeChangeLogin = () => {
-    navigate("/login");
-  };
-  const routeChangeSignUp = () => {
-    navigate("/signup");
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
   };
 
   return (
-    <Nav className={"Navbar " + theme}>
-      <div className="logo">
-        <a href="/">
-          <h1>Little Moments</h1>
-        </a>
-      </div>
-      <div className="RouteBtn">
-        <Button variant="outline-dark" onClick={routeChangeAbout}>
-          About Us
-        </Button>
-        <Button variant="outline-dark" onClick={routeChangeLists}>
-          Lists
-        </Button>
-        <Button variant="outline-dark" onClick={routeChangeProducts}>
-          Products
-        </Button>
-      </div>
-      <Button variant="outline-dark" onClick={toggleTheme}>
-        {theme === "light" ? "Dark 🌜" : "Light 🟡"}
-      </Button>
-      <div id="UserBtn">
-        {!isLoggedIn && (
-          <>
-            <Button variant="outline-dark" onClick={routeChangeLogin}>
-              Login
-            </Button>
-            <Button variant="outline-dark" onClick={routeChangeSignUp}>
-              SignUp
-            </Button>{" "}
-          </>
-        )}
+    <div className={"Navbar " + theme}>
+      <Nav>
+        <div className="logo">
+          <a href="/">
+            <h1>Little Moments</h1>
+          </a>
+        </div>
+        <ul>
+          <li>
+            <a href="/about">About Us</a>
+          </li>
+          <li>
+            <a href="/lists">Lists</a>
+          </li>
+          <li>
+            <a href="/products">Products</a>
+          </li>
+          {!isLoggedIn && (
+            <>
+              <li>
+                <a href="/login">Login </a>
+              </li>
+              <li>
+                <a href="/signup">SignUp</a>
+              </li>
+            </>
+          )}
+          {isLoggedIn && <li onClick={logOutUser}>Log Out</li>}
+        </ul>
 
-        {isLoggedIn && (
-          <>
-            <Button variant="outline-danger" onClick={logOutUser}>
-              Logout
-            </Button>
-            <div id="userMessage">
-              <p>{user && "Hello, " + user.name}</p>
-            </div>
-          </>
-        )}
-      </div>
-    </Nav>
+        <Button variant="outline-dark" onClick={toggleTheme}>
+          {theme === "light" ? "Dark 🌜" : "Light 🟡"}
+        </Button>
+
+        <>
+          <div className="hamburger" onClick={toggleHamburger}>
+            <Hamburger isOpen={hamburgerOpen} />
+          </div>
+        </>
+      </Nav>
+    </div>
   );
 }
 
