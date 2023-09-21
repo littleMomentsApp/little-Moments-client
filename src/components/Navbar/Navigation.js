61f71f9a8ddd4f84ba4e65831d3c2565636faadd
Lucas Silva <gaviao.lucas@gmail.com>
2023-09-21 20:02:23 +0100

    updating navbar

--- src/components/Navbar/Navigation.js
+++ src/components/Navbar/Navigation.js
@@ -2,6 +2,7 @@ import NavLinks from "./NavLinks";
 import classes from "./navbar.module.css";
 import { useContext } from "react";
 import { ThemeContext } from "../../context/theme.context";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
 import React from "react";
 
 function Navigation() {
@@ -10,7 +11,11 @@ function Navigation() {
     <nav className={classes.Navigation}>
       <NavLinks />
       <button className={classes.themebtn} onClick={toggleTheme}>
-        {theme === "light" ? "🌜" : "🟡"}
+        {theme === "light" ? (
+          <BsMoonFill color="#333" />
+        ) : (
+          <BsSunFill color="#333" size="20px" />
+        )}
       </button>
     </nav>
   );
